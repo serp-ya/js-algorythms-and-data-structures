5266301e5c42fb8f239c0d9caed59737faa3c03e
Sergey G. Kundryukov <skundryukov@tsc.ts>
2018-10-05 12:05:04 +0300

    Add tail & add head methods

--- data-structures/linked-list/index.js
+++ data-structures/linked-list/index.js
@@ -7,10 +7,34 @@
 function LinkedList() {
   this.head = null;
   this.tail = null;
-}
+};
 
-function ListNode(value, next, prev) {
+function ListNode(value, prev, next) {
   this.value = value;
-  this.next = next;
   this.prev = prev;
-}
+  this.next = next;
+};
+
+LinkedList.prototype.addToHead = function (value) {
+  var newNode = new ListNode(value, null, this.head);
+  
+  if (this.head) {
+    this.head.prev = newNode;
+  } else {
+    this.tail = newNode;
+  }
+
+  this.head = newNode;
+};
+
+LinkedList.prototype.addToTail = function (value) {
+  var newNode = new ListNode(value, this.tail, null);
+  
+  if (this.tail) {
+    this.tail.next = newNode;
+  } else {
+    this.head = newNode;
+  }
+
+  this.tail = newNode;
+};
